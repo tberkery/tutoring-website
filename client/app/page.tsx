2fa8d25ee70adfcd1ab2e9e9bbe62c450a4a7bb2
NolanFogarty <nolan.k.fogarty@gmail.com>
2024-02-26 18:17:53 -0500

    [feat]: hello world profile page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import HelloWorld from "../components/HelloWorld";
 import "../styles/global.css";
+import Profile from "./profile/page";
 
 const Page : FC = () => {
   return (
