51719da976638b9a68666b829192b2518100db9d
NolanFogarty <nolan.k.fogarty@gmail.com>
2024-03-02 18:35:31 -0500

    [feat] edit profile synced with backend, properly edits / updates existing profiles

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -34,17 +34,16 @@ const Profile = () => {
     return <div className="text-center mt-20">No profile data found</div>;
   }
 
-  console.log(profileData);
-
   return (
     <div className="flex justify-between items-center bg-blue-300 pt-16 pb-16 pl-32 pr-32">
       <div className="flex-1 mr-5">
-        <h1 className="text-2xl font-bold">{profileData[0].firstName} {profileData[0].lastName} - {profileData[0].department}, {profileData[0].graduationYear}</h1>
-        <p className="text-s underline font-light mb-2">{profileData[0].email}</p>
-        <p className="text-gray-700 text-base">{profileData[0].description}</p>
+        <h1 className="text-2xl font-bold">{profileData.data[0].firstName} {profileData.data[0].lastName} - {profileData.data[0].department}
+                                           {profileData.data[0].graduationYear ? `, ${profileData.data[0].graduationYear}` : ''}</h1>
+        <p className="text-s underline font-light mb-2">{profileData.data[0].email}</p>
+        <p className="text-gray-700 text-base">{profileData.data[0].description}</p>
       </div>
       <div className="flex-none pl-32 flex flex-col items-center">
-        <img className="w-268 h-268 md:w-48 md:h-48 snap-center rounded-md" src={profileData.profilePic} alt={`${profileData.name}`} />
+        <img className="w-268 h-268 md:w-48 md:h-48 snap-center rounded-md" src={profileData && profileData.data[0].picture ? profileData.data[0].picture : 'defaultimg.jpeg'} alt={`${profileData.name}`} />
         <div className="flex mt-4 space-x-4">
           <Link href="/profile/edit" passHref>
                       <button className="bg-custom-blue hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-md">
