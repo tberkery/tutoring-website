2a7715d0bff995e16a1016d98e450bf4aca483c9
katherineforbes <katanneforbes@gmail.com>
2024-03-02 19:36:58 -0500

    [fix]: removed dist files

--- server/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//server/routes/index.ts
-const { Router } = require('express');
-const router = Router();
-//router.use('/user', require('./user'))
-router.use('/courses', require('./courses.ts'));
-router.use('/posts', require('./posts.ts'));
-router.use('/profiles', require('./profiles.ts'));
-module.exports = router;
--- server/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// server/server.ts
-const app = require('./app.ts');
-const PORT = process.env.PORT || 6300;
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}...`);
-});
