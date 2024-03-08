382c0e50db94adb37207ebd016ebe3ba17b25d81
dokker19 <dkyang1023@gmail.com>
2024-03-08 13:41:18 -0500

    [chore] attempt #42

--- server/index.ts
+++ server/index.ts
@@ -1,46 +1,13 @@
 // server/server.ts
-
+export {}
 require('dotenv').config();
 const PORT = process.env.PORT
-
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const connect = require('./data/db');
 //import connect from './data/db';
-import router from './routes/index';
-
-// class App {
-
-//     public app: any;
-
-//     constructor(){
-//         this.app = express()
-//         //this.setupMiddlewares()
-//         //this.dbConnection()
-//         //this.setRouting()
-//     }
-
-//     setupMiddlewares() {
-//         this.app.get('/', (req: any, res: any) => {
-//             res.send('Hello World!')
-//         })
-//         this.app.use(cors()); 
-//         this.app.use(express.json())
-//         this.app.use(bodyParser.json())
-//         this.app.use(bodyParser.urlencoded({ extended: false }))
-//     }
-
-//     async dbConnection(){
-//         await connect()
-//     }
-
-//     setRouting() {
-//         this.app.use(router)
-//     }
-// }
-
+//import router from './routes/index';
 
 
 const app = express()
@@ -55,8 +22,9 @@ app.get('/', (req: any, res: any) => {
     res.json("Hello!")
 })
 
-app.use(router);
 
+
+connect();
 // (async () => {
 //   await connect();
 // })();
