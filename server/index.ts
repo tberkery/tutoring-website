7b406501a720fd9c5742dcd98a83addc4e0da7ce
dokker19 <dkyang1023@gmail.com>
2024-03-08 13:15:30 -0500

    [chore] attemtping deployment with app import

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,9 @@
 // server/server.ts
 
-// const app = require('./app.ts')
-// const PORT = process.env.PORT || 6300;
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port: ${PORT}...`);
-// });
-
-// module.exports = app;
-const express = require('express')
-const app = express();
 require('dotenv').config();
 
-
-const PORT=process.env.PORT
-console.log(PORT)
+const app = require('./app.ts')
+const PORT = process.env.PORT
 
 app.get('/', (req: any, res: any) => {
     res.json("Hello!")
