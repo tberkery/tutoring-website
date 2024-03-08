e554bfdf4a4eb0348042c0fd9ebb0b2b835c838c
dokker19 <dkyang1023@gmail.com>
2024-03-08 13:18:07 -0500

    [chore] attemting deployment without app import

--- server/app.ts
+++ server/app.ts
@@ -1,43 +1,43 @@
-//server/app.js
-require("dotenv").config({ path: "./config.env" });
+// //server/app.js
+// require("dotenv").config({ path: "./config.env" });
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors');
-import connect from './data/db';
-import router from './routes/index';
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors');
+// import connect from './data/db';
+// import router from './routes/index';
 
-class App {
+// class App {
 
-    public app: any;
+//     public app: any;
 
-    constructor(){
-        this.app = express()
-        this.setupMiddlewares()
-        //this.dbConnection()
-        //this.setRouting()
-    }
+//     constructor(){
+//         this.app = express()
+//         //this.setupMiddlewares()
+//         //this.dbConnection()
+//         //this.setRouting()
+//     }
 
-    setupMiddlewares() {
-        this.app.get('/', (req: any, res: any) => {
-            res.send('Hello World!')
-        })
-        this.app.use(cors()); 
-        this.app.use(express.json())
-        this.app.use(bodyParser.json())
-        this.app.use(bodyParser.urlencoded({ extended: false }))
-    }
+//     setupMiddlewares() {
+//         this.app.get('/', (req: any, res: any) => {
+//             res.send('Hello World!')
+//         })
+//         this.app.use(cors()); 
+//         this.app.use(express.json())
+//         this.app.use(bodyParser.json())
+//         this.app.use(bodyParser.urlencoded({ extended: false }))
+//     }
 
-    async dbConnection(){
-        await connect()
-    }
+//     async dbConnection(){
+//         await connect()
+//     }
 
-    setRouting() {
-        this.app.use(router)
-    }
-}
+//     setRouting() {
+//         this.app.use(router)
+//     }
+// }
 
-module.exports = new App().app
+// module.exports = new App().app
 
 
 
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,53 @@
 // server/server.ts
 
 require('dotenv').config();
-
-const app = require('./app.ts')
 const PORT = process.env.PORT
+
+
+//server/app.js
+require("dotenv").config({ path: "./config.env" });
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+import connect from './data/db';
+import router from './routes/index';
+
+class App {
+
+    public app: any;
+
+    constructor(){
+        this.app = express()
+        //this.setupMiddlewares()
+        //this.dbConnection()
+        //this.setRouting()
+    }
+
+    setupMiddlewares() {
+        this.app.get('/', (req: any, res: any) => {
+            res.send('Hello World!')
+        })
+        this.app.use(cors()); 
+        this.app.use(express.json())
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.urlencoded({ extended: false }))
+    }
+
+    async dbConnection(){
+        await connect()
+    }
+
+    setRouting() {
+        this.app.use(router)
+    }
+}
+
+
+
+const app = new App().app
+
+
 
 app.get('/', (req: any, res: any) => {
     res.json("Hello!")
