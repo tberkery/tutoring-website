3369d59f59531430fc22a1c273b9253a1d2589ff
dokker19 <dkyang1023@gmail.com>
2024-03-08 13:50:01 -0500

    [chore] attempt #46

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,5 @@
 //server/routes/index.ts
-
+export {}
 const { Router } = require('express')
 const router = Router()
 
@@ -9,7 +9,7 @@ router.use('/courses', require('./courses.ts'))
 
 router.use('/posts', require('./posts.ts'))
 
-//router.use('/profiles', require('./profiles.ts'))
+router.use('/profiles', require('./profiles.ts'))
 
 router.use('/profilePics', require('./profilePics.ts'))
 
