418d7ebf46637f09baa4892c7c3ab3fb1efcd66b
dokker19 <dkyang1023@gmail.com>
2024-03-08 14:04:11 -0500

    [chore] attempt #50

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,7 +5,18 @@ const router = Router()
 
 //router.use('/user', require('./user'))
 
-router.use('/courses', require('./courses'))
+// router.use('/courses', require('./courses'))
+const CourseDaoClass = require('../data/CourseDao');
+const CourseDao = new CourseDaoClass();
+router.get("/courses/all", async (req: any, res: any ) => {
+    try {
+      const courses = await CourseDao.readAll();
+      res.status(200).json({ courses });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error");
+    }
+  });
 
 // router.use('/posts', require('./posts.ts'))
 
