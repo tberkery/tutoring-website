104e9d14fffa08ccdd448ac860469c5afb85aab1
dokker19 <dkyang1023@gmail.com>
2024-03-08 13:07:59 -0500

    [chore] attemp with just server

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,27 @@
 // server/server.ts
 
-const app = require('./app.ts')
-const PORT = process.env.PORT || 6300;
+// const app = require('./app.ts')
+// const PORT = process.env.PORT || 6300;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}...`);
+// });
+
+// module.exports = app;
+const express = require('express')
+const app = express();
+require('dotenv').config();
+
+
+const PORT=process.env.PORT
+console.log(PORT)
+
+app.get('/', (req: any, res: any) => {
+    res.json("Hello!")
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}...`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
