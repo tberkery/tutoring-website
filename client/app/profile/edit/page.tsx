90542aedb25d3f5b595a5f2ab97e1146c3abbc63
Matth-ewe-f <matthewtf73@gmail.com>
2024-03-09 19:50:00 -0500

    setup first e2e test

--- client/app/profile/edit/page.tsx
+++ client/app/profile/edit/page.tsx
@@ -5,7 +5,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import Navbar from "../../../components/Navbar";
-import { Button } from "../../../@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -14,8 +14,8 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "../../../@/components/ui/form";
-import { Input } from "../../../@/components/ui/input";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 const MAX_BIO_LENGTH = 200;
 
 const formSchema = z.object({
@@ -29,7 +29,7 @@ const formSchema = z.object({
 
 type ProfileFormData = z.infer<typeof formSchema>;
 
-export function EditProfile() {
+export default function EditProfile() {
   const form = useForm<ProfileFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -151,4 +151,3 @@ export function EditProfile() {
   );
 }
 
-export default EditProfile;
