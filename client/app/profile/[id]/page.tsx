481bd752ac498caed2737b2c0ab5a60ab8e0cd5a
NolanFogarty <nolan.k.fogarty@gmail.com>
2024-03-17 04:35:45 -0400

    [fix] minor change to unused file

--- client/app/profile/[id]/page.tsx
+++ client/app/profile/[id]/page.tsx
@@ -2,14 +2,11 @@
 import React from 'react';
 import "../../../styles/global.css";
 import Navbar from "@/components/Navbar"
-import Profile from "@/components/Profile"
-import PostsSection from '@/components/PostSection';
 
 const Page: React.FC = ({ params }: { params : { id: string }}) => {
   return (
     <>
       <Navbar />
-      <Profile profileId={params.id}/>
       {/* <PostsSection /> */}
     </>
   );
