29bc5d7a0a429d0995dcab12e5d5017faaf46931
NolanFogarty <nolan.k.fogarty@gmail.com>
2024-03-26 17:11:45 -0400

    [feat] added MUI components, updated posts to include hard-coded ratings

--- client/components/PostCard.tsx
+++ client/components/PostCard.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react';
 import { useRouter } from "next/navigation";
+import StarIcon from '@mui/icons-material/Star';
+import Link from 'next/link';
 
 interface Post {
   _id: string;
@@ -38,22 +40,28 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
   }
 
   return ( <> 
-    <div className="max-w-sm overflow-hidden py-2" onClick={handleClick}>
+    <div className="max-w-sm overflow-hidden py-2">
       <img
-        className="w-full aspect-ratio: 4 / 3 object-cover rounded"
+        className="w-full aspect-ratio: 4 / 3 object-cover rounded cursor-pointer"
         src={post.imageUrl || defaultImage}
         alt="Post Image"
+        onClick={handleClick}
       />
       <div className="px-2 py-2">
         <div className="py-0.5">
-            <div className="text-2xl font-bold font-sans text-slate-700 uppercase">{post.courseName ? post.courseName : post.activityTitle}</div>
+            <div className="text-2xl font-bold font-sans text-slate-700 uppercase truncate">{post.courseName ? post.courseName : post.activityTitle}</div>
             <p className="text-slate-500 text-sm font-sans">{post.courseNumber}</p>
         </div>
-        <div className="py-1 flex justify-between"> 
-          <p className="text-slate-600 text-sm font-sans">From ${post.price}</p>
-          <p className="text-slate-600 text-sm font-sans">Created by <a href={`/profile/` + post.userId} className="font-semibold">User</a>{post.username}</p>
+        <p className="text-slate-800 text-base font-sans line-clamp-2 min-h-12 hover:underline cursor-pointer	" onClick={handleClick}>{post.description ? post.description : post.activityDescription}</p>
+        <div className="pt-1 flex justify-between "> 
+          <p className="text-black text-sm font-sans font-bold">From ${post.price}</p>
+          <p className="text-black text-sm font-sans">Created by <a href={`/profile/` + post.userId} className="font-semibold">User</a>{post.username}</p>
         </div>
-        <p className="text-slate-800 text-base font-sans">{post.description ? post.description : post.activityDescription}</p>
+      </div>
+      <div className="ratings flex mx-2">
+        <StarIcon className="text-black-300 text-sm" />
+        <h1 className="font-bold pt-0.25 pr-1">5.0</h1>
+        <Link href="/reviews" className="text-slate-500">(72)</Link>
       </div>
     </div>
     </>
