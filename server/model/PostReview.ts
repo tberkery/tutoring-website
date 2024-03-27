c8434237e0fc20f3f3250b3b78a91efb128be2df
katherineforbes <katanneforbes@gmail.com>
2024-03-27 16:04:15 -0400

    [chore] created post review schema

---
+++ server/model/PostReview.ts
@@ -0,0 +1,23 @@
+export {}
+
+import mongoose from "mongoose";
+
+const PostReviewSchema = new mongoose.Schema({
+    postId: {           // id of the post that the review was written for
+      type: String,
+      required: true,
+    },
+    userId: {           // id of the user that wrote the review
+        type: String,
+        required: true,
+    },
+    review: {
+      type: String,
+      required: true,
+      default: null,
+    },
+});
+
+const PostReview = mongoose.model("Post", PostReviewSchema);
+
+module.exports = PostReview;
