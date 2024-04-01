4ccf3cc67cd3c26507e0534e0e9f397438677870
Matth-ewe-f <matthewtf73@gmail.com>
2024-04-01 15:26:31 -0400

    reviews ui on others profile

---
+++ client/components/ReviewCard.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { FC, HTMLAttributes, useEffect, useRef, useState } from 'react';
+import RatingStars from './RatingStars';
+import { text } from 'stream/consumers';
+
+type review = {
+  title: string,
+  rating: number,
+  leftBy: string,
+  post: string,
+  text: string
+}
+
+type props = {
+  review: review,
+} & HTMLAttributes<HTMLDivElement>
+
+const ReviewCard : FC<props> = (props) => {
+  const review = props.review;
+  const textRef = useRef<HTMLParagraphElement>(null);
+  const [showFull, setShowFull] = useState(false);
+  const [isClamped, setIsClamped] = useState(false);
+
+  const isTextClamped = (element : Element) => {
+    return element.scrollHeight > element.clientHeight;
+  }
+
+  useEffect(() => { setIsClamped(isTextClamped(textRef.current)) }, [])
+
+  return (
+    <div className={`${props.className} px-4 py-3`}>
+      <div className='flex justify-between'>
+        <h3 className='text-2xl font-bold'>{review.title}</h3>
+        <p className='text-sm mt-0.5 text-gray-800'>
+          Left by <span className='font-bold'>{review.leftBy}</span>
+        </p>
+      </div>
+      <RatingStars rating={review.rating}/>
+      <p className='text-sm mt-1 text-gray-800'>
+        Review of <span className='font-bold'>{review.post}</span>
+      </p>
+      <p 
+        className={`mt-1 ${showFull ? '' : 'line-clamp-2'}`} ref={textRef}
+      >
+        {review.text}
+      </p>
+      { isClamped ?
+        <div className='flex justify-center'>
+          { showFull ? 
+            <button 
+              className='text-sm text-gray-500 mt-1'
+              onClick={() => setShowFull(false)}
+            >
+              Hide Full Review
+            </button>
+          :
+            <button 
+              className='text-sm text-gray-500 mt-1'
+              onClick={() => setShowFull(true)}
+            >
+              Show Full Review...
+            </button>
+          }
+        </div>
+      :
+        ''
+      }
+    </div>
+  );
+}
+
+export default ReviewCard;
