860e21edda3a389202c102e72ef5a1769198428e
Matth-ewe-f <matthewtf73@gmail.com>
2024-04-01 15:31:26 -0400

    reviews tab on own profile

--- client/components/ReviewCard.tsx
+++ client/components/ReviewCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { FC, HTMLAttributes, useEffect, useRef, useState } from 'react';
 import RatingStars from './RatingStars';
-import { text } from 'stream/consumers';
 
 type review = {
   title: string,
@@ -32,12 +31,16 @@ const ReviewCard : FC<props> = (props) => {
       <div className='flex justify-between'>
         <h3 className='text-2xl font-bold'>{review.title}</h3>
         <p className='text-sm mt-0.5 text-gray-800'>
-          Left by <span className='font-bold'>{review.leftBy}</span>
+          Left by <span className='font-bold cursor-pointer hover:underline'>
+            {review.leftBy}
+          </span>
         </p>
       </div>
       <RatingStars rating={review.rating}/>
       <p className='text-sm mt-1 text-gray-800'>
-        Review of <span className='font-bold'>{review.post}</span>
+        Review of <span className='font-bold cursor-pointer hover:underline'>
+          {review.post}
+        </span>
       </p>
       <p 
         className={`mt-1 ${showFull ? '' : 'line-clamp-2'}`} ref={textRef}
@@ -48,14 +51,14 @@ const ReviewCard : FC<props> = (props) => {
         <div className='flex justify-center'>
           { showFull ? 
             <button 
-              className='text-sm text-gray-500 mt-1'
+              className='text-sm text-gray-500 mt-1 hover:underline'
               onClick={() => setShowFull(false)}
             >
               Hide Full Review
             </button>
           :
             <button 
-              className='text-sm text-gray-500 mt-1'
+              className='text-sm text-gray-500 mt-1 hover:underline'
               onClick={() => setShowFull(true)}
             >
               Show Full Review...
