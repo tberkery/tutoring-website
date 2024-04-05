c2009e7bcaeb0fb79cc481740f5bfb4dba5bb3cf
dokker19 <dkyang1023@gmail.com>
2024-04-05 17:53:23 -0400

    [feat] Chat UI color update

--- client/components/Chat.jsx
+++ client/components/Chat.jsx
@@ -8,10 +8,18 @@ import { useUser } from "@clerk/nextjs";
 
 const APP_ID = process.env.NEXT_PUBLIC_SEND_BIRD_APP_ID;
 
+const tutorHubColorSet = {
+   '--sendbird-light-primary-500': '#adc9ff',
+   '--sendbird-light-primary-400': '#adc9ff',
+   '--sendbird-light-primary-300': '#adc9ff',
+   '--sendbird-light-primary-200': '#adc9ff',
+   '--sendbird-light-primary-100': '#adc9ff',
+};
+
 export default function Chat({ userId }) {
    return (
       <div style={{ height: "100vh", width: "100vw" }}>
-         <SendbirdApp appId={APP_ID} userId={userId} />
+         <SendbirdApp appId={APP_ID} userId={userId} colorSet={tutorHubColorSet}/>
       </div>
    );
 }
