b86b23c2bb785f9867b4f7bf7e784e602e12635b
Matth-ewe-f <matthewtf73@gmail.com>
2024-04-10 15:51:28 -0400

    [bug] added test dynamic page with no other functionality

---
+++ client/app/[test]/page.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import "../styles/global.css";
+
+const Page : FC = ({ params }: { params : { test: string }}) => {
+  return <>
+    <p>{params.test}</p>
+  </>;
+};
+
+export default Page;
