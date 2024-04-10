c7d03d570dda493cc5a31b136c9f3a4bfada0eee
Matth-ewe-f <matthewtf73@gmail.com>
2024-04-10 14:14:22 -0400

    [bug] removed chat page

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,49 +1,47 @@
-// pages/index.js
+// 'use client';
+// import Head from 'next/head'
+// //import styles from '../styles/Home.module.css'
+// import dynamic from "next/dynamic";
+// import '@sendbird/uikit-react/dist/index.css';
+// import { useUser } from "@clerk/nextjs";
+// import NavBar from '@/components/Navbar';
 
-'use client';
-import Head from 'next/head'
-//import styles from '../styles/Home.module.css'
-import dynamic from "next/dynamic";
-import '@sendbird/uikit-react/dist/index.css';
-import { useUser } from "@clerk/nextjs";
-import NavBar from '@/components/Navbar';
+// const DynamicAppWithNoSSR = dynamic(() => import("../../components/Chat"), {
+//   ssr: false,
+//   loading: () => <p>...</p>
+// });
 
-const DynamicAppWithNoSSR = dynamic(() => import("../../components/Chat"), {
-  ssr: false,
-  loading: () => <p>...</p>
-});
+// export default function Home() {
+//     const { isLoaded, isSignedIn, user } = useUser();
+//     let email = '';
+//     let jhed_id = '';
 
-export default function Home() {
-    const { isLoaded, isSignedIn, user } = useUser();
-    let email = '';
-    let jhed_id = '';
+//     if (user && user.primaryEmailAddress) {
+//         email = user.primaryEmailAddress.toString();
+//         const atIndex = email.indexOf('@');
+//         if (atIndex !== -1 && email.endsWith('@jhu.edu')) {
+//             jhed_id = email.substring(0, atIndex);
+//             console.log(jhed_id);
+//         } else {
+//             console.log('Invalid email format');
+//         }
+//     } else {
+//         console.log('Email address not available');
+//     }
 
-    if (user && user.primaryEmailAddress) {
-        email = user.primaryEmailAddress.toString();
-        const atIndex = email.indexOf('@');
-        if (atIndex !== -1 && email.endsWith('@jhu.edu')) {
-            jhed_id = email.substring(0, atIndex);
-            console.log(jhed_id);
-        } else {
-            console.log('Invalid email format');
-        }
-    } else {
-        console.log('Email address not available');
-    }
-
-  return (
-    <>
-      <NavBar/>
-      <Head>
-        <title>UIKit with NextJS 13</title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-      {/* <main className={styles.main}> */}
-        <DynamicAppWithNoSSR userId={jhed_id}/>
-      </main>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <NavBar/>
+//       <Head>
+//         <title>UIKit with NextJS 13</title>
+//         <meta name="description" content="" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <main>
+//       {/* <main className={styles.main}> */}
+//         <DynamicAppWithNoSSR userId={jhed_id}/>
+//       </main>
+//     </>
+//   )
+// }
