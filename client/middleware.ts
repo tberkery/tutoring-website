9a498f607662ba6b2410849d975402306516c2a0
Matth-ewe-f <matthewtf73@gmail.com>
2024-04-11 15:23:37 -0400

    [bug] added log in middleware for deployment

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
  
 export default authMiddleware({
   publicRoutes: ['/', '/createAccount', '/signIn']
@@ -7,3 +8,8 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+export function middleware(req: NextRequest) {
+  console.log(req.url);
+  return NextResponse.next();
+}
